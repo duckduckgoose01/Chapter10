73432bfb54b035a96095fb447ed64d71af3ce1d7
duckduckgoose01 <davidshapiro@brandeis.edu>
2022-10-23 00:42:57 -0400

    Initial load

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+    before_save { self.email = email.downcase }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true, length:  { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 250 }
+    validates :email, format: {with: VALID_EMAIL_REGEX },
+                      uniqueness: true
+                    
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+    end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,20 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  include ApplicationHelper
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Add more helper methods to be used by all tests here...
+end
